e17cc89b51b1e9fe31a7cc734d842be446188dd6
sugarnova <103873202+sugarnova@users.noreply.github.com>
2023-03-08 23:22:14 +0530

    initial commit

---
+++ src/components/MIdElement.tsx
@@ -0,0 +1,266 @@
+import React from 'react'
+import "../styles/midelement.css";
+import { MdOutlineRefresh} from 'react-icons/md';
+import { BiDotsVerticalRounded} from 'react-icons/bi';
+
+const MidElement = () => {
+  const data= [
+    {
+      "icon": "star_border",
+      "icons2": "star_border",
+      "title": "youtube",
+      "message": "on Your Channel Future Coders",
+      "decription": "on Your Channel Future Coders"
+},
+{
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},
+{
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},  {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},
+{
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+}, {
+  "icon": "star_border",
+  "icons2": "star_border",
+  "title": "youtube",
+  "message": "on Your Channel Future Coders",
+  "decription": "on Your Channel Future Coders"
+},
+];
+   
+  return (
+    <>
+    <div className="emaillist">
+      {/* <!-- Settings Starts --> */}
+      <div className="emailList_settings">
+        <div className="emailList_settingsLeft">
+          <input type="checkbox" />
+          <span className="material-icons"> <MdOutlineRefresh/></span>
+          <span className="material-icons"> <BiDotsVerticalRounded/></span>
+          <span className="material-icons"> redo</span>
+          <span className="material-icons"> more_vert</span>
+        </div>
+
+        <div className="emailList_settingsRight">
+          <span className="material-icons"> chevron_left</span>
+          <span className="material-icons"> chevron_left</span>
+          <span className="material-icons"> keyboard_hide</span>
+          <span className="material-icons"> settings</span>
+        </div>
+
+      </div>
+ 
+      {/* <div className="emaillist_sections">
+        <div className="section section__selected">
+          <span className="material-icons"> inbox </span>
+          <h4>Primary</h4>
+        </div>
+
+       
+        <div className="section">
+          <span className="material-icons"> people </span>
+          <h4>Social</h4>
+        </div>
+
+   
+        <div className="section">
+          <span className="material-icons"> local_offer </span>
+          <h4>Promotions</h4>
+        </div>
+    </div> */}
+    {/* // Section Ends */}
+
+    {/* // Email List Row Starts */}
+    <div className="emailList_list">
+      
+      {/* Email Row Starts */}
+   
+    
+      {
+        data.map((item)=>{
+          return(
+            <div className="emailRow">
+            <div className="emailRow__options">
+              <input type = "checkbox" />
+              <span className= "material-icons"> {item.icon}</span>
+              <span className = "material-icons"> {item.icons2}</span>
+            </div>
+            <h3 className ="emailRow_title">{item.title}</h3>
+      
+            <div className="emailRow__message">
+              <h4>
+                {item.message} <span className="emailRow__description"> {item.decription}</span>
+                </h4>
+            </div>
+      
+            <p className ="emailRow__time">10pm</p>
+      
+          </div>
+          )
+        })
+      }
+           
+          
+        
+      
+    
+
+    
+
+      {/* Email Row Ends */}
+
+    </div>
+    {/* // Email List Row Ends */}
+    </div>
+      </>
+      
+  )
+}
+
+
+export default MidElement
