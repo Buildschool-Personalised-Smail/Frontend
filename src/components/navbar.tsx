45213d601c4c8647f13da29e3778689fd7c4f26e
ashjaysiv <96778394+ashjaysiv@users.noreply.github.com>
2023-03-11 18:46:08 +0530

    position adjust, added support

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../styles/navbar.css';
 import gmailicon from "../assets/icons8-gmail.svg";
+import filter from "../assets/filter-6551.svg";
+import pencil from "../assets/pencil-svgrepo-com.svg";
+import drive from "../assets/icons8-google-drive.svg";
+import docs from "../assets/Google_Docs_logo_(2014-2020).svg";
+import sheets from "../assets/Google_Sheets_logo_(2014-2020).svg";
+import slides from "../assets/icons8-google-slides.svg";
+import calendar from "../assets/Google_Calendar_icon_(2020).svg"; 
+import meet from "../assets/Google_Meet_icon_(2020).svg";
+import groups from "../assets/google-groups-seeklogo.com.svg";
+import youtube from "../assets/YouTube_full-color_icon_(2017).svg";
+import photos from "../assets/Google_Photos_icon_(2020).svg";
+import classroom from "../assets/Google_Classroom_Logo.svg";
+import search from "../assets/icons8-search.svg";
+import iit from "../assets/IIT-Madras-01.svg"
 
 const Navbar = () => {
 
@@ -47,6 +61,26 @@ const Navbar = () => {
         // e.preventDefault();
         document.getElementById("app_d")?.classList.toggle("show");
     }
+
+    window.onclick = function(event){
+        // event.preventDefault();
+        if (!(event.target as HTMLInputElement)?.matches('.material-symbols-outlined')){
+            var dropdowns = document.getElementsByClassName("support-dropdown");
+            var i;
+            for ( i= 0 ; i<dropdowns.length; i++){
+                var openDropdown = dropdowns[i];
+                if(openDropdown.classList.contains('show')){
+                    
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+    function support(){
+        //event.preventDefault();
+        document.getElementById("support")?.classList.toggle("show");
+    }
     return(
     <body>
         <nav className= "navbar">
@@ -62,9 +96,12 @@ const Navbar = () => {
             </div>
 
             <div className = "nav-search">
-                <button id="a"><span className="material-symbols-outlined" id="b"> search </span></button>
-                <input type="text" placeholder="Search in mail" /> 
-                <button id="a"><img src="../assests/filter-6551.svg" className="filter"/></button>
+                <button id="a"><img src= {search} className = "search"></img></button>
+                <div className= "search-box">
+                    <input type="text" placeholder="Search in mail" /> 
+                </div> 
+                
+                <button id="a"><img src= {filter} className="filter"/></button>
             </div>
             <div className = "down">
                 <button onClick={dropdown} className="active" ><span className="greendot"></span>  Active  <span className= "down-arrow"></span></button>
@@ -94,7 +131,7 @@ const Navbar = () => {
                             </div>
                         </a>
                         <a id= "status">
-                            <img src="../assests/pencil-svgrepo-com.svg" className="edit"/>
+                            <img src={pencil} className="edit"/>
                             <div className = "options">
                                 Add a status
                             </div>
@@ -103,7 +140,14 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="nav-icons">
-                <button className="material-symbols-outlined" id="a">help</button>
+                <button onClick={support} className="material-symbols-outlined" id="a">help</button>
+                
+                <div className = "support-dropdown" id="support">
+                    <a className="support-text">Help</a>
+                    <a className="support-text">Send Feedback</a>
+                </div>
+                
+                
 
                 <button className="material-symbols-outlined" id="a">settings</button>
 
@@ -119,59 +163,59 @@ const Navbar = () => {
                                 <div className = "app-text">Account</div>
                             </div>
                             <div onClick= {()=>{window.location.href ='https://mail.google.com'}} className = "app-rep">
-                                <img src="../assests/icons8-gmail.svg" alt="gmail"/>
+                                <img src={gmailicon} alt="gmail"/>
                                 <div className = "app-text">
                                     Gmail
                                 </div>
                             </div>
                             <div className = "app-rep" onClick= {()=>{window.location.href = 'https://drive.google.com'}}>
-                                <img src="../assests/icons8-google-drive.svg" alt="drive"/>
+                                <img src={drive} alt="drive"/>
                                 <div className = "app-text">Drive</div>
                             </div>
                         </div>
                         
                         <div className = "three">
                             <div className="app-rep">
-                                <img src="../assests/Google_Docs_logo_(2014-2020).svg" alt="docs" onClick={()=>{window.location.href='https://docs.google.com/'}}/>
+                                <img src={docs} alt="docs" onClick={()=>{window.location.href='https://docs.google.com/'}}/>
                                 <div className = "app-text">Docs</div>
                             </div>
                             <div className = "app-rep">
-                                <img src="../assests/Google_Sheets_logo_(2014-2020).svg" alt="sheets" onClick = {() =>{window.location.href='https://docs.google.com/spreadsheets/'}}/>
+                                <img src={sheets} alt="sheets" onClick = {() =>{window.location.href='https://docs.google.com/spreadsheets/'}}/>
                                 <div className = "app-text">Sheets</div>
                             </div>
                             <div className = "app-rep" >
-                                <img src="../assests/icons8-google-slides.svg" alt="slides" onClick = {()=>{window.location.href  = 'https://docs.google.com/presentation/' }}/> 
+                                <img src={slides} alt="slides" onClick = {()=>{window.location.href  = 'https://docs.google.com/presentation/' }}/> 
                                 <div className = "app-text">Slides</div>
                             </div>
                         </div>
 
                         <div className = "three">
                             <div className="app-rep">
-                                <img src="../assests/Google_Calendar_icon_(2020).svg" alt="calendar" className="calendar" onClick= {()=>{window.location.href = 'https://calendar.google.com/'}}/>
+                                <img src={calendar} alt="calendar" className="calendar" onClick= {()=>{window.location.href = 'https://calendar.google.com/'}}/>
                                 <div className = "app-text">Calendar</div>
                             </div>
                             <div className = "app-rep">
-                                <img src="../assests/Google_Meet_icon_(2020).svg" alt="meet" className="calendar" onClick= {()=>{window.location.href= 'https://meet.google.com/'}}/>
+                                <img src={meet} alt="meet" className="calendar" onClick= {()=>{window.location.href= 'https://meet.google.com/'}}/>
                                 <div className = "app-text">Meet</div>
                             </div>
                             <div className = "app-rep" >
-                                <img src="../assests/google-groups-seeklogo.com.svg" alt="groups" onClick = {()=>{window.location.href = 'https://groups.google.com/'}}/>
+                                <img src={groups} alt="groups" onClick = {()=>{window.location.href = 'https://groups.google.com/'}}/>
                                 <div className = "app-text">Groups</div>
                             </div>
                         </div>
 
                         <div className = "three">
                             <div className="app-rep">
-                                <img src="../assests/YouTube_full-color_icon_(2017).svg" alt="youtube" className="calendar" onClick = {() =>{window.location.href = 'https://www.youtube.com/'}}/>
+                                <img src={youtube} alt="youtube" className="calendar" onClick = {() =>{window.location.href = 'https://www.youtube.com/'}}/>
                                 <div className = "app-text">Youtube</div>
                             </div>
                             <div className = "app-rep">
-                                <img src="../assests/Google_Photos_icon_(2020).svg" alt="photos" className="calendar" onClick = {()=>{window.location.href = 'https://photos.google.com/'}}/>
+                                <img src={photos} alt="photos" className="calendar" onClick = {()=>{window.location.href = 'https://photos.google.com/'}}/>
                                 <div className = "app-text">Photos</div>
                             </div>
                             <div className = "app-rep" >
-                                <img src="../assests/Google_ClassNameroom_Logo.svg" alt="ClassNameroom" className="calendar" onClick = {()=>{window.location.href = 'https://classNameroom.google.com/'}}/>
-                                <div className = "app-text">ClassNameroom</div>
+                                <img src={classroom} alt="Classroom" className="calendar" onClick = {()=>{window.location.href = 'https://classroom.google.com/'}}/>
+                                <div className = "app-text">Classroom</div>
                             </div>
                         </div>
 
@@ -181,9 +225,9 @@ const Navbar = () => {
                 
 
                 <div className="account">
-                    <button id="account">
+                    <button id="account" onClick = {() =>{window.location.href="https://mail.google.com/"}}>
                         <div className="insti">
-                            <img src="../assests/IIT-Madras-01.svg"/> 
+                            <img src={iit}/> 
                             <span className="iitm">IITM</span>
                         </div>
                         <div className="dot">
