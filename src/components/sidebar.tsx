9a651c49451658474ba789139f3bd1b18a6130d2
Shaunak <ch21b062@smail.iitm.ac.in>
2023-03-12 21:30:34 +0530

    icons added lol

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import '../styles/sidebar.css';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
+import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 const SideBar = () => {
 
@@ -11,41 +19,41 @@ const SideBar = () => {
             <div className="left-bar">
               <button style={{border: "none"}}>
                 <div id="compose" className="compose-item">
-                  <span className="material-icons-outlined"> edit </span>
+                  <EditOutlinedIcon className="icons" />
                   <span className="compose-hidden">Compose</span>
                 </div>
               </button>
               <div className="features">
       
                 <div className="feature-items" id="inbox">
-                  <span className="material-icons"> inbox </span>
+                  <MailOutlinedIcon className="icons" />
                   <span className="hidden-feature"><strong>Inbox</strong></span>
                   <span className="no-message">6969</span>
                 </div>
       
                 <div className="feature-items" id="starred">
-                  <span className="material-icons-outlined"> grade </span>
+                  <GradeOutlinedIcon className="icons" />
                   <span className="hidden-feature">Starred</span>
                 </div>
       
                 <div className="feature-items" id="snoozed">
-                  <span className="material-icons-outlined"> watch_later </span>
+                  <WatchLaterOutlinedIcon className='icons' />
                   <span className="hidden-feature">Snoozed</span>
                 </div>
       
                 <div className="feature-items" id="send">
-                  <span className="material-icons-outlined"> send </span>
+                  <SendOutlinedIcon className='icons' />
                   <span className="hidden-feature">Sent</span>
                 </div>
       
                 <div className="feature-items" id="drafts">
-                  <span className="material-icons-outlined"> description </span>
+                  <DescriptionOutlinedIcon className='icons' />
                   <span className="hidden-feature"><strong>Drafts</strong></span>
                   <span className="no-message">69</span>
                 </div>
       
                 <div className="feature-items" id="more">
-                  <span className="material-icons-outlined"> expand_more </span>
+                  <ExpandMoreOutlinedIcon className='icons' />
                   <span className="hidden-feature">More</span>
                 </div>
       
@@ -53,7 +61,9 @@ const SideBar = () => {
       
               <div className="labels">
                 <span className="hidden-label">Label</span>
-                <button style={{border: "none"}} className="material-icons-outlined"> add </button>
+                <button style={{border: "none"}}>
+                  <AddOutlinedIcon className="icons" />
+                </button>
               </div>
             </div>
         </main>
